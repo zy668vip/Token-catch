caad3a34cf5b817326c111885a7e627e03830cfa
zy668vip <zy668vip@gmail.com>
2025-01-10 02:05:42 +0800

    Update 提取token.js

--- 提取token.js
+++ 提取token.js
@@ -10,54 +10,56 @@
 // @downloadURL  https://github.com/zy668vip/Token-catch/raw/main/%E6%8F%90%E5%8F%96token.js
 // ==/UserScript==
 
-(function() {
-    'use strict';
+function getRandomiOSUserAgent() {
+    const iOSVersions = [
+                            '15_0', '15_1', '15_2', '15_3', '15_4', '15_5', '15_6', '15_7',
+                            '16_0', '16_1', '16_2', '16_3', '16_4', '16_5', '16_6', '16_7',
+                            '17_0', '17_1', '17_2', '17_3', '17_4', '17_5'];
+    const iPhoneModels = [ 'iPhone11,8', 'iPhone12,1',
+                            'iPhone12,3', 'iPhone12,5', 'iPhone13,1', 'iPhone13,2', 'iPhone13,3',
+                            'iPhone13,4', 'iPhone14,2', 'iPhone14,3', 'iPhone14,4', 'iPhone14,5'];
+    const randomVersion = iOSVersions[Math.floor(Math.random() * iOSVersions.length)];
+    const randomModel = iPhoneModels[Math.floor(Math.random() * iPhoneModels.length)];
+    return `Mozilla/5.0 (${randomModel}; CPU iPhone OS ${randomVersion} like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1`;
+}
 
-    function createButton(copyText, buttonId, buttonText, buttonPosition) {
-        if (document.getElementById(buttonId)) return;
-
-        const button = document.createElement('button');
-        button.id = buttonId;
-        button.textContent = buttonText;
-        button.style.position = 'fixed';
-        button.style[buttonPosition.vertical] = '10px';
-        button.style[buttonPosition.horizontal] = '10px';
-        button.style.padding = '10px';
-        button.style.backgroundColor = '#007bff';
-        button.style.color = '#ffffff';
-        button.style.border = 'none';
-        button.style.borderRadius = '5px';
-        button.style.cursor = 'pointer';
-        button.style.zIndex = '10000';
-
-        button.addEventListener('click', function() {
-            const textarea = document.createElement('textarea');
-            textarea.value = copyText;
-            document.body.appendChild(textarea);
-            textarea.select();
-            try {
-                document.execCommand('copy');
-            } catch (err) {
-                console.error('复制到剪贴板失败:', err);
-            }
-            document.body.removeChild(textarea);
-        });
-
-        document.body.appendChild(button);
-    }
-
-    function extractToken() {
-        const token = localStorage.getItem('token');  // 从本地存储中获取 token
-        if (token) {
-            // 创建并显示复制按钮
-            createButton(token, 'token-copy-button', '复制 token', { vertical: 'bottom', horizontal: 'left' });
-        } else {
-            console.log('在本地存储中未找到 token');
+function emitIphone() {
+    function updateIframeLink () {
+        const url = location.href
+        if (!url.startsWith('https://')) {
+            return setTimeout(updateIframeLink, 500)
+        }
+        if (url.includes('tgWebAppPlatform')) {
+            location.href = url.replace(/tgWebAppPlatform=[a-z]*\&/g, 'tgWebAppPlatform=ios&')
+            console.dir('[GBOT] Updated HREF:', location.href)
         }
     }
+    updateIframeLink()
 
-    // 确保文档已完全加载后再执行
-    window.onload = function() {
-        extractToken();
-    };
-})();
+    Object.defineProperty(navigator, 'userAgent', {
+      get: function() { return getRandomiOSUserAgent() }
+    })
+    Object.defineProperty(navigator, 'platform', {
+        get: function() { return 'iPhone' }
+    })
+    Object.defineProperty(navigator, 'vendor', {
+        get: function() { return 'Apple Computer, Inc.' }
+    });
+    Object.defineProperty(navigator, 'deviceMemory', {
+        get: function() { return undefined }
+    });
+    Object.defineProperty(navigator, 'maxTouchPoints', {
+        get: function() { return 5 }
+    })
+}
+
+function mobileCheck() {
+    const text = document.body?.innerText
+    return text && text.replaceAll('\n', ' ').match(/(Play|Use).*(mobile|smartphone)/gi)
+}
+
+function onStart() {
+    if (mobileCheck()) return setTimeout(() => location.reload())
+}
+emitIphone()
+setTimeout(onStart, 2000)
